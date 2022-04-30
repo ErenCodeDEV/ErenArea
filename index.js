2de80fc35e4c6fd3515fb59cf665d8088d5bfba1
ErenCodeDEV <104595496+ErenCodeDEV@users.noreply.github.com>
2022-04-30 11:23:11 +0200

    Apploadfiles

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
+
+client.login("OTY5NjYzNTM2MDA1NTE3Mzcy.Ymwrig.tZOEsyYi7cSmXquQBVHhMR1_CCY")
